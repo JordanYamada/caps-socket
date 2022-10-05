7f1e06374cdc06e8e1af232725faba18279e3579
Jordan Yamada <sadaotomokane@gmail.com>
2022-10-05 00:25:59 +0000

    changed start point for live deploy

--- src/Vendor/index.js
+++ src/Vendor/index.js
@@ -3,7 +3,7 @@
 const io = require('socket.io-client');
 const handleVendor = require('./handleVendor.js');
 
-const URL = process.env.CAPS_URL || 'http://localhost:3002';
+const URL = process.env.CAPS_URL || 'http://localhost:3002/caps';
 const socket = io.connect(URL);
 
 socket.on('delivered', handleVendor(socket));
